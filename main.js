fe962b7256464255e8e240292b0feab1f9dc2f06
Nikitik Nikita <nik.ikraaaa@gmail.com>
2019-12-16 20:39:56 +0300

    Lesson_1 HW_1

---
+++ main.js
@@ -0,0 +1,19 @@
+//Задание №1
+let name;
+let manager;
+let currentUser;
+
+name = "Вася";
+manager = name;
+currentUser = manager;
+
+
+//Задание №5
+var animal = {
+    type: "dog",
+    weight: 40,
+};
+animal.color = "brown";
+animal.speed = 25;
+delete animal.weight;
+animal.color = "ginger";
