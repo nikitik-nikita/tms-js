ae7a39b029f6251de3af490aa0fe37faca58e9b1
Nikitik Nikita <nik.ikraaaa@gmail.com>
2020-01-11 22:15:16 +0300

    Lesson_4 HW_4 (with classwork task)

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const data = [
 
 function showResult(lastName) {
     for (let i = 0; i < data.length; i++) {
-        if (data[i].lastName.toLocaleLowerCase() === lastName) {
+        if (data[i].lastName.toLocaleLowerCase() === lastName.toLocaleLowerCase()) {
             return data[i];
         }
 
@@ -59,7 +59,7 @@ input.addEventListener("keyup", function (event) {
         if (typeof (result) === 'object') {
             let html = '';
             for (let key in result) {
-                html += key +': '+ result[key]+ '<br>' ;
+                html += key + ': ' + result[key] + '<br>';
             }
             document.getElementById('showResultInput').innerHTML = html;
         } else {
@@ -67,3 +67,74 @@ input.addEventListener("keyup", function (event) {
         }
     }
 );
+
+
+//Домашнее задание_3 (classwork_4).
+//(classwork_4)
+const poly = 'репер';
+
+function showResultOfPolyndrom() {
+    let arr = poly.split('');
+    arr = arr.reverse().join('');
+    return (poly === arr) ? 'это палиндром' : 'это не палиндром';
+}
+
+console.log(showResultOfPolyndrom());
+
+
+//1.
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+console.log(min(2, 4));
+
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
+console.log(min(2, 1));
+
+function max(a, b) {
+    if (a < b) {
+        return b;
+    } else {
+        return a;
+    }
+
+}
+
+console.log(max(9, 8));
+
+function max(a, b) {
+    return (a > b) ? a : b;
+}
+
+console.log(max(9, 10));
+
+//2.
+let arr = [0, 1, 24, 20, 40, 66, 100, 77, 70, 88];
+let newArr = [];
+let resultOfChange = function changeValue(arr) {
+    arr.forEach(function (item, i, arr) {
+        if (item === 0) {
+            newArr.push('zero')
+        } else if (item % 100 === 0) {
+            newArr.push(item / 100 + 'zerozero')
+        } else if (item % 10 === 0) {
+            newArr.push(item / 10 + 'zero')
+        } else {
+            newArr.push(item)
+        }
+
+    });
+    return newArr;
+};
+
+console.log(resultOfChange(arr));
