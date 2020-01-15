471dbab6f574e3628679acd133f2f10cb6b3a125
Nikitik Nikita <nik.ikraaaa@gmail.com>
2020-01-15 19:01:26 +0300

    lesson_5 HW_4 Задание_2 (доделан)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -158,28 +158,21 @@ const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
 const text3 = document.getElementById('text3');
 
-const changeTextColor = (element) => {
-    for (i = 0; i <= colors.length;) {
-        return function () {
-            text1.style.color = colors[i];
-            // text2.style.color = colors[i];
-            // text3.style.color = colors[i];
-            i++;
-            if (i > colors.length) {
-                i = 0;
-            }
+const changeTextColor = () => {
+    let i = 0;
+    return function (event) {
+        event.target.style.color = colors[i];
+        i++;
+        if (i > colors.length) {
+            i = 0;
         }
     }
-
-    // text2.style.color = '';
-    // text3.style.color = '';
-
-
-    // text2.style[key] = value;
-    // text3.style[key] = value;
-
 };
-// const changeColor = changeTextColor('color', text1);
-text1.addEventListener('click', changeTextColor(text1));
-text1.addEventListener('click', changeTextColor(text2));
-text1.addEventListener('click', changeTextColor(text3));
+const changeColor1 = changeTextColor();
+const changeColor2 = changeTextColor();
+const changeColor3 = changeTextColor();
+
+text1.addEventListener('click', changeColor1);
+text2.addEventListener('click', changeColor2);
+text3.addEventListener('click', changeColor3);
+
